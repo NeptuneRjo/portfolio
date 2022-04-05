70aaf6fcf59555022aa403d792b35dded0ccaacf
NeptuneRjo <rjo9000@outlook.com>
2022-04-05 19:50:14 -0400

    added json and mapped over projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import projectsjson from '../../assets/projects.json';
+import Project from './Project/Project';
 
 const Projects = () => {
+
+  const projects = projectsjson.projects
+
   return (
-    <div>Projects</div>
+    <div className="projects-main">
+      <div className="projects-header">
+        <h4>Projects</h4>
+      </div>
+      <div className="projects-content">
+        {
+          projects.map((project, index) => (
+            <Project project={project} id={index} key={index} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
