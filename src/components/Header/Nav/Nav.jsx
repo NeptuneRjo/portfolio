347adaeefd539762e9737a8ffd664c78ba15f379
NeptuneRjo <rjo9000@outlook.com>
2022-04-05 16:45:54 -0400

    added and styled mobile nav

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css';
+
+import { 
+  RiMenu3Line,
+  RiCloseLine
+} from 'react-icons/ri';
+
 
 const Nav = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div>Nav</div>
+    <div className="nav-main">
+      <div className="nav-desktop">
+      </div>
+      <div className="nav-mobile">
+        {toggleMenu
+          ? <RiCloseLine 
+              color='#66FCF1' 
+              size={27} 
+              onClick={() => setToggleMenu(false)}
+            />
+          : <RiMenu3Line
+              color='#66FCF1'
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />            
+        }
+      </div>
+      <div className={`nav-mobile-menu ${toggleMenu}`}>
+        <div className="nav-mobile-items slide-bottom">
+          <div className="nav-mobile-item">
+            <p><a href='#about'>About</a></p>
+          </div>
+          <div className="nav-mobile-item">
+            <p><a href='#projects'>Projects</a></p>
+          </div>
+          <div className="nav-mobile-item">
+            <p><a href='#contact'>Contact</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
