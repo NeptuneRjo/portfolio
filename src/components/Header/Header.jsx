2a5647748f44981cb52ce3bdfa651b44c3f9ec5c
NeptuneRjo <rjo9000@outlook.com>
2022-04-05 17:00:50 -0400

    added and styled header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './style.css';
+import Nav from './Nav/Nav'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header-main">
+      <div className="header-title">
+        <h4>Ryan O'Connell</h4>
+        <p>Frontend Developer</p>
+      </div>
+      <div className="header-nav">
+        <Nav />
+      </div>
+    </div>
   )
 }
 
