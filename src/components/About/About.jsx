ebb65fdad654d11ef8b7ec28838353ff69b52114
NeptuneRjo <rjo9000@outlook.com>
2022-04-05 18:16:10 -0400

    added jsx for about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about-main" id="about">
+      <div className="about-content">
+        <h4>About <span>Me</span></h4>
+        <p className='fade-in-1'>Hello!</p>
+        <p className='fade-in-2'>My name is <span>Ryan O'Connell </span>
+         and I'm a software developer that specializes on the <span>Frontend.</span></p> 
+        <p className='fade-in-3'>My job is to create responsive and accessible front-end web applications.</p>
+      </div>
+    </div>
   )
 }
 
