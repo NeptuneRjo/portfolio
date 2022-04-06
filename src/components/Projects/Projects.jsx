fd90f89d77c26ba5a6eaa9dbcf265dded401c8d2
NeptuneRjo <rjo9000@outlook.com>
2022-04-06 14:21:06 -0400

    added projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import projectsjson from '../../assets/projects.json';
 import Project from './Project/Project';
+import './style.css'
 
 const Projects = () => {
 
   const projects = projectsjson.projects
 
   return (
-    <div className="projects-main">
+    <div className="projects-main" id='projects'>
       <div className="projects-header">
-        <h4>Projects</h4>
+        <h4>Take a look at my <span>projects:</span></h4>
       </div>
-      <div className="projects-content">
+      <div className='projects-content'>
         {
           projects.map((project, index) => (
-            <Project project={project} id={index} key={index} />
+            <Project project={project} id={index} />
           ))
         }
       </div>
