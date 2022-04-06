83229cf6c00ca8a8b2b0f11f4dde34b42790ad17
NeptuneRjo <rjo9000@outlook.com>
2022-04-06 17:41:10 -0400

    removed animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,11 +5,12 @@ const About = () => {
   return (
     <div className="about-main" id="about">
       <div className="about-content">
-        <h4>About <span>Me</span></h4>
-        <p className='fade-in-1'>Hello!</p>
-        <p className='fade-in-2'>My name is <span>Ryan O'Connell </span>
-         and I'm a software developer that specializes on the <span>Frontend.</span></p> 
-        <p className='fade-in-3'>My job is to create responsive and accessible front-end web applications.</p>
+        <h4><span>About</span> Me</h4>
+        <p>
+          Hello! <br />
+          My name is <span>Ryan O'Connell </span>
+          and I'm a software developer that specializes on the <span>Frontend.</span> <br />
+          My job is to create responsive and accessible front-end web applications.</p>
       </div>
     </div>
   )
