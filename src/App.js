ccbda8770ca356711dbc064157ed662088bf9443
NeptuneRjo <rjo9000@outlook.com>
2022-04-14 18:04:42 -0400

    moved menu for easier styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-import { About, Contact, Header, Projects } from './components';
+import { About, Contact, Hero, Projects } from './components';
+import { ParallaxProvider } from 'react-scroll-parallax'
+import Menu from './components/Menu/Menu';
+
 
 function App() {
   return (
-    <div className="app-main">
-      <Header />
-      <About />
-      <Projects />
-      <Contact />
-    </div>
+    <ParallaxProvider>
+      <div className="app-main">
+        <Menu />
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
+    </ParallaxProvider>
   );
 }
 
