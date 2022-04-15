aaf5dfa11487f58dabdf514058cdc9bb3dbf7cbe
NeptuneRjo <rjo9000@outlook.com>
2022-04-15 00:27:33 -0400

    setup projects for mobile

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,21 +1,65 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import projectsjson from '../../assets/projects.json';
-import Project from './Project/Project';
 import './style.css'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Projects = () => {
+
+  useEffect(() => {
+    Aos.init({duration: 750});
+  }, [])
 
   const projects = projectsjson.projects
 
   return (
     <div className="projects-main" id='projects'>
-      <div className="projects-header">
-        <h4>Take a look at my <span>projects:</span></h4>
-      </div>
       <div className='projects-content'>
+        <h4>My Projects</h4>
         {
           projects.map((project, index) => (
-            <Project project={project} id={index} />
+            <div 
+              className="projects-item" 
+              id={index} 
+              key={index}
+              data-aos='fade-up'
+            >
+              <h5>{project.name}</h5>
+              <p className="projects-description">
+                {project.description}
+              </p>
+              <div className="projects-images">
+                <img 
+                  src={project.desktop} 
+                  alt='projects desktop view' 
+                  className='projects-desktop' 
+                />
+                <img 
+                  src={project.mobile} 
+                  className='projects-mobile' 
+                  alt='projects mobile view'
+                />
+              </div>
+              <div className="projects-links">
+                <div className="projects-github blue__button">
+                  <a 
+                    href={project.links.github} 
+                    target="_blank"
+                  >
+                    Github
+                  </a>
+                </div>
+                <div className="projects-live white__button">
+                  <a 
+                    href={project.links.live} 
+                    target="_blank"
+                  >
+                    Live Preview
+                  </a>
+                </div>
+              </div>
+            </div>
           ))
         }
       </div>
