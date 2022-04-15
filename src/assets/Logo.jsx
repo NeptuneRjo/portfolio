b062d453a0bedda7f6eaeb725d6f0249248b63df
NeptuneRjo <rjo9000@outlook.com>
2022-04-15 18:25:24 -0400

    created logo

---
+++ src/assets/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="logo-main">
+        <div className="logo-container">
+            RO
+        </div>
+    </div>
+  )
+}
+
+export default Logo
