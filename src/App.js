3fb5301a0f65bca5507f3b9d2fa71d189c9da116
NeptuneRjo <rjo9000@outlook.com>
2022-06-19 02:02:22 -0400

    cleaned up imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
-import { About, Contact, Hero, Projects } from './components';
+import './App.css'
+import { About, Contact, Hero, Projects, Menu } from './components'
 import { ParallaxProvider } from 'react-scroll-parallax'
-import Menu from './components/Menu/Menu';
-
 
 function App() {
-  return (
-    <ParallaxProvider>
-      <div className="app-main">
-        <Menu />
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-      </div>
-    </ParallaxProvider>
-  );
+	return (
+		<ParallaxProvider>
+			<div className='app-main'>
+				<Menu />
+				<Hero />
+				<About />
+				<Projects />
+				<Contact />
+			</div>
+		</ParallaxProvider>
+	)
 }
 
-export default App;
+export default App
